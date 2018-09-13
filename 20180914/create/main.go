178cd02302955eef6ec90042c425645f999aead1
Takuya Fujimoto <t.fujimoto@gemcook.com>
2018-09-14 01:33:49 +0900

    [remove] ### Unnecessary gem of model

--- 20180914/create/main.go
+++ 20180914/create/main.go
@@ -1,29 +1,10 @@
 package main
 
 import (
-	"time"
-
 	"github.com/gemcook/study-go/20180914/misc"
 	"github.com/gemcook/study-go/20180914/models"
 	_ "github.com/go-sql-driver/mysql"
 )
-
-// Gem is model struct
-type Gem struct {
-	ID            int       `xorm:"id pk autoincr"`
-	Name          string    `xorm:"name"`
-	JapaneseName  string    `xorm:"japanese_name"`
-	EnglishName   string    `xorm:"english_name"`
-	Hardness      float32   `xorm:"hardness"`
-	Price         int       `xorm:"price"`
-	ProducingArea string    `xorm:"producing_area"`
-	Carat         float32   `xorm:"carat"`
-	Weight        float32   `xorm:"weight"`
-	Color         string    `xorm:"color"`
-	Memo          string    `xorm:"memo"`
-	Scratched     bool      `xorm:"scratched"`
-	MiningDate    time.Time `xorm:"mining_date updated"`
-}
 
 // TableName is function
 func (Gem) TableName() string {
