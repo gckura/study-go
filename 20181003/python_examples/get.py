5b35dd84932b4ffff7267cc6f5890729ef397eee
hidaka <n.hidaka@gemcook.com>
2018-10-02 19:13:02 +0900

    [update] b-6 pip、requestsのコメント追加

--- 20181003/python_examples/get.py
+++ 20181003/python_examples/get.py
@@ -1,4 +1,9 @@
+# -- coding: utf-8 --
 import requests
+
+# 1. `sudo easy_install pip` で pip をインストールする。
+# 2. `pip install requests --user` で requests をインストールする。
+# 上記の手順で requests というライブラリが使用できるようになる。
 
 response = requests.get('https://api.tech.gemcook.org/v1/gems')
 print(response.status_code)
