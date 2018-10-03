647de5e37e69653c06e7dff499de52ca48d9a3d8
hidaka <n.hidaka@gemcook.com>
2018-10-03 19:50:32 +0900

    [update] b-6 コメント追加

--- 20181003/go_examples/get_gems/main.go
+++ 20181003/go_examples/get_gems/main.go
@@ -1,5 +1,9 @@
 package main
 
+// fmtパッケージ 標準出力に出力する
+// io/ioutilパッケージ 入出力関連の関数が定義されている
+// net/httpパッケージ HTTPを扱うパッケージで、HTTPクライアントとHTTPサーバーを実装するために必要な機能が提供されている
+
 import (
 	"fmt"
 	"io/ioutil"
@@ -7,6 +11,8 @@ import (
 )
 
 func main() {
+	// HTTP GETリクエストを送信
+	// レスポンスはhttp.Get関数の戻り値として取得することができます。
 	resp, err := http.Get("https://api.tech.gemcook.org/v1/gems")
 
 	if err != nil {
@@ -14,8 +20,11 @@ func main() {
 		return
 	}
 
+	// レスポンスを受け取ったら必ずBodyをCloseするのが決まり
 	defer resp.Body.Close()
 
+	// b = buffer のこと
+	// レスポンスから内容を取得する(resp.Body)
 	b, err := ioutil.ReadAll(resp.Body)
 	if err == nil {
 		fmt.Println(string(b))
--- 20181003/python_examples/get.py
+++ 20181003/python_examples/get.py
@@ -1,10 +1,20 @@
 # -- coding: utf-8 --
 import requests
+# pip --versionでpipコマンドがなかった場合
 
 # 1. `sudo easy_install pip` で pip をインストールする。
 # 2. `pip install requests --user` で requests をインストールする。
 # 上記の手順で requests というライブラリが使用できるようになる。
 
+# - requests とは -
+# Python の HTTP ライブラリ。これ一つでGETもPOSTもできる
+
+# requestsの requests.getでデータを取得する
 response = requests.get('https://api.tech.gemcook.org/v1/gems')
+
+# ステータスコードを標準出力に出力する
 print(response.status_code)
+
+# レスポンスのテキストを標準出力に出力する
 print(response.text)
+
